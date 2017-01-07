de815392f045bcfd9c3543118c83ff7ed83f8678
Ricardo Mendes <ricardo.2.mendes@atos.net>
2017-01-07 12:20:15 -0200

    Account CRUD

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,11 +2,62 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AccountRequest;
+use App\Model\Account;
+use App\Model\AccountType;
 use Illuminate\Http\Request;
 
 class AccountController extends Controller
 {
+
+	/**
+	 * @var Account
+	 */
+	private $account;
+
+	/**
+	 * @var AccountType
+	 */
+	private $accountType;
+
+	public function __construct(Account $account, AccountType $accountType)
+	{
+		$this->account = $account;
+		$this->accountType = $accountType;
+	}
+
 	public function index()
+	{
+		$accounts = $this->account->all();
+		return view('layouts.account_index', compact('accounts'));
+    }
+
+	public function create()
+	{
+		$method = 'POST';
+		$route = route('account.store');
+		$account = new $this->account;
+		$accountTypes = $this->accountType->pluck('description', 'id')->all();
+
+		return view('layouts.account_store', compact('method', 'route', 'account', 'accountTypes'));
+	}
+
+	public function store(AccountRequest $request)
+	{
+		dd($request);
+    }
+
+	public function edit($id)
+	{
+
+    }
+
+	public function update(AccountRequest $request)
+	{
+
+    }
+
+	public function destroy($id)
 	{
 
     }
---
+++ resources/views/layouts/account_store.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('content')
+	@if (count($errors) > 0)
+		<div class="alert alert-danger">
+			<ul>
+				@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+				@endforeach
+			</ul>
+		</div>
+	@endif
+
+	<form method="post" action="{{$route}}">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="form-group">
+					<label for="account">Conta</label>
+					<input type="text" class="form-control" id="account" name="account" placeholder="Conta"
+						   value="{{$account->name}}">
+					<input type="hidden" name="id" value="{{$account->id}}">
+					{{ csrf_field() }}
+					{{ method_field($method) }}
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="form-group">
+					<label for="type">Tipo da Conta</label>
+					<select name="type" class="form-control">
+						<option value="invalid_option">Selecione</option>
+						@foreach($accountTypes as $accountTypeId => $accountType)
+							<option value="{{$accountTypeId}}">{{$accountType}}</option>
+						@endforeach
+					</select>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class=" form-group">
+					<button type="submit" name="save" value="save" class="btn btn-success">Salvar</button>
+					<a href="{{route('account.index')}}" class="btn btn-danger">Cancelar</a>
+				</div>
+			</div>
+		</div>
+	</form>
+
+@endsection
