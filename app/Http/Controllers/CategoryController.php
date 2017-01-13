c8e284f3f8d9932692bf370464155771b9ba42ab
Ricardo Mendes <ricardo.2.mendes@atos.net>
2017-01-13 00:00:52 -0200

    Modal to create category while importing extract

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -9,70 +9,91 @@ use Session;
 class CategoryController extends Controller
 {
 
-    /**
-     * @var Category
-     */
-    private $category;
+	/**
+	 * @var Category
+	 */
+	private $category;
 
-    public function __construct(Category $category)
-    {
-        $this->category = $category;
-    }
+	public function __construct(Category $category)
+	{
+		$this->category = $category;
+	}
 
-    public function index()
-    {
-        $categories = $this->category->all();
-        return view('layouts.category_index', compact('categories'));
-    }
+	public function index()
+	{
+		$categories = $this->category->all();
+		return view('layouts.category_index', compact('categories'));
+	}
 
-    public function create()
-    {
-        $category = new $this->category;
-        $route = route('category.store');
-        $method = 'POST';
-        return view('layouts.category_store', compact('category', 'route', 'method'));
-    }
+	public function create()
+	{
+		$category = new $this->category;
+		$route = route('category.store');
+		$method = 'POST';
+		return view('layouts.category_store', compact('category', 'route', 'method'));
+	}
 
-    public function store(CategoryRequest $request)
-    {
+	public function store(CategoryRequest $request)
+	{
+		$message = 'Categoria criada com sucesso!';
+		$messageAjax = 'Categoria Criada! Você já pode seleciná-la!';
+		$messageType = 'success';
+		$categoryName = $request->input('category');
+		$success = true;
+		$existentCategory = $this->category->where('name', $categoryName)->first();
+		if (is_null($existentCategory)) {
+			$category = new $this->category;
+			$category->name = $categoryName;
+			$category->save();
+		} else {
+			$category = $existentCategory;
+			$message = $messageAjax = "Categoria '{$categoryName}' já existe!";
+			$messageType = 'info';
+			$success = false;
+		}
 
-        $category = new $this->category;
-        $category->name = $request->input('category');
-        $category->save();
+		if ($request->ajax())
+			return json_encode([
+					'success' => $success,
+					'category_id' => $category->id,
+					'category_name' => $category->name,
+					'message' => $messageAjax
+				]
+			);
 
-        Session::flash('success', 'Categoria criada com sucesso!');
-        return redirect()->route('category.index');
-    }
+		Session::flash($messageType, $message);
+		return redirect()->route('category.index');
+	}
 
-    public function edit($id)
-    {
-        $category = $this->category->find($id);
-        $route = route('category.update', ['id' => $id]);
-        $method = 'PUT';
-        return view('layouts.category_store', compact('category', 'route', 'method'));
-    }
+	public function edit($id)
+	{
+		$category = $this->category->find($id);
+		$route = route('category.update', ['id' => $id]);
+		$method = 'PUT';
+		return view('layouts.category_store', compact('category', 'route', 'method'));
+	}
 
-    public function update(CategoryRequest $request, $id)
-    {
-        $category = $this->category->find($id);
-        $category->name = $request->input('category');
-        $category->save();
+	public function update(CategoryRequest $request, $id)
+	{
+		$category = $this->category->find($id);
+		$category->name = $request->input('category');
+		$category->save();
 
-        Session::flash('success', 'Categoria atualizada com sucesso!');
-        return redirect()->route('category.index');
-    }
+		Session::flash('success', 'Categoria atualizada com sucesso!');
+		return redirect()->route('category.index');
+	}
 
-    public function destroy($id)
-    {
-        $category = $this->category->find($id);
-        if ($category->transactions->count() == 0) {
-            $category->delete();
-            Session::flash('success', 'Categoria removida com sucesso!');
-        } else {
-            Session::flash('info', 'Categoria "' . $category->name . '"" não pode ser excluída: Existem transações vinculadas.');
-        }
+	public function destroy($id)
+	{
+		$category = $this->category->find($id);
+		if ($category->transactions->count() == 0) {
+			$category->delete();
+			Session::flash('success', 'Categoria removida com sucesso!');
+		} else {
+			Session::flash('info', 'Categoria "'.$category->name.'"" não pode ser excluída: Existem transações vinculadas.');
+		}
 
-        return redirect()->route('category.index');
-    }
+		return redirect()->route('category.index');
+	}
 
 }
